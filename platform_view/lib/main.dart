3bdde425e7fa72038bec850f4142f673cfc3589d
ronjb <ron@tecarta.com>
2019-07-16 05:52:27 -0600

    update to show bugs

--- platform_view/lib/main.dart
+++ platform_view/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -7,27 +6,61 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    return MaterialApp(
+        home: MyHomePage(
+      showWebView: false,
+      level: 0,
+    ));
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final bool showWebView;
+  final int level;
+
+  const MyHomePage({Key key, this.showWebView, this.level}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => MyHomePage())),
-            )
-          ],
-        ),
-        body: Stack(
-          children: <Widget>[
-            WebView(initialUrl: 'https://google.com'),
-            const Placeholder(),
-          ],
-        ),
-      );
+  Widget build(BuildContext context) {
+    Widget buildButton(String title, bool showWebView) => FlatButton(
+          child: Text(
+            title,
+            style: TextStyle(color: Colors.white),
+          ),
+          onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => MyHomePage(
+                showWebView: showWebView,
+                level: level + 1,
+              ),
+            ),
+          ),
+        );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('$level'),
+        actions: <Widget>[
+          buildButton('+ webview', true),
+          buildButton('+ regular', false),
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          showWebView
+              ? WebView(
+                  initialUrl:
+                      'https://pub.dev/packages/webview_flutter#-readme-tab-')
+              : Center(
+                  child: Text(
+                    'Text widget',
+                    textScaleFactor: 3,
+                  ),
+                ),
+          const Placeholder(),
+        ],
+      ),
+    );
+  }
 }
